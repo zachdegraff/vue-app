987c8973d3fe941198c00370d112676e231f6297
ahmetdoner7 <donerahmet093@gmail.com>
2018-05-01 18:17:36 +0300

    Initialize

---
+++ src/layouts/default.vue
@@ -0,0 +1,62 @@
+<template>
+    <q-layout>
+        <q-layout-header>
+            <q-toolbar color="primary">
+                <q-toolbar-title>
+                    EAST BAY MECH
+                </q-toolbar-title>
+                <q-toolbar-title>
+                    Wonderus
+                </q-toolbar-title>
+                <q-btn color="white" class="text-black">
+                    <q-icon name="add"/>
+                    Add Card
+                </q-btn>
+                <div class="auth-user q-ml-md">
+                    <img src="~assets/profile.jpg" class="auth-user-image round-borders vertical-middle"/>
+                    <q-popover>
+                        <q-list separator link>
+                            <q-item>
+                                <q-item-main>
+                                    <q-item-tile>My Profile</q-item-tile>
+                                </q-item-main>
+                            </q-item>
+                            <q-item>
+                                <q-item-main>
+                                    <q-item-tile>Logout</q-item-tile>
+                                </q-item-main>
+                            </q-item>
+                        </q-list>
+                    </q-popover>
+                </div>
+            </q-toolbar>
+        </q-layout-header>
+
+        <q-page-container>
+            <router-view/>
+        </q-page-container>
+
+        <q-layout-footer>
+
+        </q-layout-footer>
+    </q-layout>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+    .auth-user-menu {
+        position: absolute;
+        right: 0;
+        top: 30px;
+    }
+
+    .auth-user-image {
+        cursor: pointer;
+        width: 35px;
+    }
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,303 @@
+<template>
+    <q-page>
+        <div class="page-search-form row justify-center bg-blue-3">
+            <q-search v-model="query" class="col-sx-12 col-md-8 bg-white q-pa-md">
+                <q-popover v-show="query===''">
+                    <div class="row">
+                        <q-list class="col">
+                            <q-list-header inset>Folders</q-list-header>
+                            <q-item>
+                                <q-item-side icon="folder" inverted color="primary"/>
+                                <q-item-main>
+                                    <q-item-tile label>Photos</q-item-tile>
+                                    <q-item-tile sublabel>February 22, 2016</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info" color="green"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="folder" inverted color="secondary"/>
+                                <q-item-main>
+                                    <q-item-tile label>Movies</q-item-tile>
+                                    <q-item-tile sublabel>March 1st, 2017</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="folder" inverted color="amber"/>
+                                <q-item-main>
+                                    <q-item-tile label>Games</q-item-tile>
+                                    <q-item-tile sublabel>Latest</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                            <q-item-separator inset/>
+                            <q-list-header inset>Files</q-list-header>
+                            <q-item>
+                                <q-item-side icon="assignment" inverted color="grey-6"/>
+                                <q-item-main>
+                                    <q-item-tile label>Expenses spreadsheet</q-item-tile>
+                                    <q-item-tile sublabel>March 2nd, 2016</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="place" inverted color="grey-6"/>
+                                <q-item-main>
+                                    <q-item-tile label>Places to visit</q-item-tile>
+                                    <q-item-tile sublabel>February 22, 2016</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info" color="amber"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="library_music" inverted color="grey-6"/>
+                                <q-item-main>
+                                    <q-item-tile label>My favorite song</q-item-tile>
+                                    <q-item-tile sublabel>Singing it all day</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="videogame_asset" inverted color="grey-6"/>
+                                <q-item-main>
+                                    <q-item-tile label>Quasar Game</q-item-tile>
+                                    <q-item-tile sublabel>Have fun while building apps</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                        </q-list>
+                        <q-list class="col">
+                            <q-list-header inset>Folders</q-list-header>
+                            <q-item>
+                                <q-item-side icon="folder" inverted color="primary"/>
+                                <q-item-main>
+                                    <q-item-tile label>Photos</q-item-tile>
+                                    <q-item-tile sublabel>February 22, 2016</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info" color="green"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="folder" inverted color="secondary"/>
+                                <q-item-main>
+                                    <q-item-tile label>Movies</q-item-tile>
+                                    <q-item-tile sublabel>March 1st, 2017</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="folder" inverted color="amber"/>
+                                <q-item-main>
+                                    <q-item-tile label>Games</q-item-tile>
+                                    <q-item-tile sublabel>Latest</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                            <q-item-separator inset/>
+                            <q-list-header inset>Files</q-list-header>
+                            <q-item>
+                                <q-item-side icon="assignment" inverted color="grey-6"/>
+                                <q-item-main>
+                                    <q-item-tile label>Expenses spreadsheet</q-item-tile>
+                                    <q-item-tile sublabel>March 2nd, 2016</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="place" inverted color="grey-6"/>
+                                <q-item-main>
+                                    <q-item-tile label>Places to visit</q-item-tile>
+                                    <q-item-tile sublabel>February 22, 2016</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info" color="amber"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="library_music" inverted color="grey-6"/>
+                                <q-item-main>
+                                    <q-item-tile label>My favorite song</q-item-tile>
+                                    <q-item-tile sublabel>Singing it all day</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="library_music" inverted color="grey-6"/>
+                                <q-item-main>
+                                    <q-item-tile label>My favorite song</q-item-tile>
+                                    <q-item-tile sublabel>Singing it all day</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="videogame_asset" inverted color="grey-6"/>
+                                <q-item-main>
+                                    <q-item-tile label>Quasar Game</q-item-tile>
+                                    <q-item-tile sublabel>Have fun while building apps</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                        </q-list>
+                        <q-list class="col">
+                            <q-list-header inset>Folders</q-list-header>
+                            <q-item>
+                                <q-item-side icon="folder" inverted color="primary"/>
+                                <q-item-main>
+                                    <q-item-tile label>Photos</q-item-tile>
+                                    <q-item-tile sublabel>February 22, 2016</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info" color="green"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="folder" inverted color="secondary"/>
+                                <q-item-main>
+                                    <q-item-tile label>Movies</q-item-tile>
+                                    <q-item-tile sublabel>March 1st, 2017</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="folder" inverted color="amber"/>
+                                <q-item-main>
+                                    <q-item-tile label>Games</q-item-tile>
+                                    <q-item-tile sublabel>Latest</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                            <q-item-separator inset/>
+                            <q-list-header inset>Files</q-list-header>
+                            <q-item>
+                                <q-item-side icon="assignment" inverted color="grey-6"/>
+                                <q-item-main>
+                                    <q-item-tile label>Expenses spreadsheet</q-item-tile>
+                                    <q-item-tile sublabel>March 2nd, 2016</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="place" inverted color="grey-6"/>
+                                <q-item-main>
+                                    <q-item-tile label>Places to visit</q-item-tile>
+                                    <q-item-tile sublabel>February 22, 2016</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info" color="amber"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="library_music" inverted color="grey-6"/>
+                                <q-item-main>
+                                    <q-item-tile label>My favorite song</q-item-tile>
+                                    <q-item-tile sublabel>Singing it all day</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                            <q-item>
+                                <q-item-side icon="videogame_asset" inverted color="grey-6"/>
+                                <q-item-main>
+                                    <q-item-tile label>Quasar Game</q-item-tile>
+                                    <q-item-tile sublabel>Have fun while building apps</q-item-tile>
+                                </q-item-main>
+                                <q-item-side right icon="info"/>
+                            </q-item>
+                        </q-list>
+                    </div>
+                </q-popover>
+                <q-autocomplete separator @search="search" class="text-black"/>
+            </q-search>
+        </div>
+        <div class="page-search-result row justify-center">
+            <q-card class="col-sx-12 col-md-8 bg-white q-mb-md">
+                <q-card-title>
+                    Cafe Basilico
+                    <div slot="right" class="row items-center">
+                        <q-icon name="place"/>
+                        250 ft
+                    </div>
+                </q-card-title>
+                <q-card-main>
+                    <p>$・Italian, Cafe</p>
+                    <p class="text-faded">Small plates, salads & sandwiches in an intimate setting.</p>
+                </q-card-main>
+                <q-card-separator/>
+                <q-card-actions align="end">
+                    <q-btn flat color="primary" label="Save" class="float-right"/>
+                </q-card-actions>
+            </q-card>
+            <q-card class="col-sx-12 col-md-8 bg-white">
+                <q-card-title>
+                    Cafe Basilico
+                    <div slot="right" class="row items-center">
+                        <q-icon name="place"/>
+                        250 ft
+                    </div>
+                </q-card-title>
+                <q-card-main>
+                    <p>$・Italian, Cafe</p>
+                    <p class="text-faded">Small plates, salads & sandwiches in an intimate setting.</p>
+                </q-card-main>
+                <q-card-separator/>
+                <q-card-actions align="end">
+                    <q-btn flat color="primary" label="Save" class="float-right"/>
+                </q-card-actions>
+            </q-card>
+        </div>
+    </q-page>
+</template>
+
+<script>
+    import {uid, filter} from 'quasar'
+    import countries from 'assets/autocomplete.json'
+
+    const icons = ['alarm', 'email', 'search', 'build', 'card_giftcard', 'perm_identity', 'receipt', 'schedule', 'speaker_phone', 'archive', 'weekend', 'battery_charging_full'];
+
+    function getRandomIcon() {
+        return icons[Math.floor(Math.random() * icons.length)]
+    }
+
+    function getRandomStamp() {
+        if (Math.floor(Math.random() * 50) % 3 === 0) {
+            return `${Math.floor(Math.random() * 10)} min`
+        }
+    }
+
+    function getRandomSecondLabel() {
+        if (Math.floor(Math.random() * 50) % 4 === 0) {
+            return `UID: ${uid().substring(0, 8)}`
+        }
+    }
+
+    function parseCountries() {
+        return countries.map(country => {
+            return {
+                label: country,
+                sublabel: getRandomSecondLabel(),
+                icon: getRandomIcon(),
+                stamp: getRandomStamp(),
+                value: country
+            }
+        })
+    }
+
+    export default {
+        name: 'PageIndex',
+        data: () => {
+            return {
+                query: ''
+            }
+        },
+        methods: {
+            search(terms, done) {
+                setTimeout(() => {
+                    done(filter(terms, {field: 'value', list: parseCountries()}))
+                }, 1000)
+            },
+        }
+    }
+</script>
+
+<style lang="scss">
+    .page-search-form {
+        padding: 100px 0;
+        position: relative;
+    }
+
+    .page-search-result {
+        position: relative;
+        margin-top: -50px;
+    }
+</style>
