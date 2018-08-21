2945b7fdd8673fd5eb9cae875f963a9e1bdac4ab
ahmetdoner7 <donerahmet093@gmail.com>
2018-08-21 17:43:12 +0400

    Install Canny on website

--- deploy.php
+++ deploy.php
@@ -1,22 +1,15 @@
 <?php
-
 namespace Deployer;
-
 require 'recipe/common.php';
-
-set('git_tty', true);
+set('git_tty', false);
 set('ssh_type', 'native');
-set('ssh_multiplexing', true);
-
+set('ssh_multiplexing', false);
 set('application', 'app');
 set('repository', 'git@github.com:jeffstern/wonderus-app.git');
-
 add('shared_files', ['dist/spa-mat/.htaccess']);
 add('shared_dirs', []);
 add('writable_dirs', []);// Hosts
-
 set('default_stage', 'dev');
-
 host('dev')
     ->hostname('ec2-18-220-139-147.us-east-2.compute.amazonaws.com')
     ->identityFile('~/.ssh/WonderusAWS.pem')
@@ -24,7 +17,6 @@ host('dev')
     ->set('branch', 'dev')
     ->set('deploy_path', '/var/www/development/{{application}}')
     ->set('stage_var', 'APP_STAGE=test');
-
 host('prod')
     ->hostname('ec2-18-220-139-147.us-east-2.compute.amazonaws.com')
     ->identityFile('~/.ssh/WonderusAWS.pem')
@@ -32,16 +24,12 @@ host('prod')
     ->set('branch', 'master')
     ->set('deploy_path', '/var/www/wonderus/{{application}}')
     ->set('stage_var', 'APP_STAGE=prod');
-
-
 task('quasar:build', function () {
     run('cd {{release_path}} && {{stage_var}} quasar build');
 });
-
 task('npm:install', function () {
     run('cd {{release_path}} && npm install');
 });
-
 task('deploy', [
     'deploy:info',
     'deploy:prepare',
@@ -54,7 +42,5 @@ task('deploy', [
     'deploy:unlock',
     'cleanup',
 ]);
-
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
-
