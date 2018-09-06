e022b16d878697c555b624a38185bda90d0bbe29
ahmetdoner7 <donerahmet093@gmail.com>
2018-09-06 14:14:01 +0400

    Text/tooltip change to CREATE A CARD button in the header

--- src/components/context/AppHeader.vue
+++ src/components/context/AppHeader.vue
@@ -32,7 +32,7 @@
                     <img src="statics/header-logo.png"/>
                 </router-link>
             </div>
-            <q-btn color="white" class="text-black gt-xs" icon="add" label="Create a card" v-if="canAddCard" @click="addCard" :disabled="isCreating"/>
+            <q-btn color="white" v-tooltip.top-center="msg" class="text-black gt-xs" icon="add" label="" v-if="canAddCard" @click="addCard" :disabled="isCreating"/>
             <q-btn color="white" class="short-add-button text-black lt-sm" icon="add" v-if="canAddCard" @click="addCard" :disabled="isCreating"/>
             <div class="auth-user q-ml-md" v-if="user">
                 <img :src="avatar(user.photo)" class="header-icon vertical-middle"/>
@@ -55,10 +55,18 @@
     </q-layout-header>
 </template>
 <script>
+    import Vue from 'vue'
     import SearchForm from '../../components/SearchForm.vue'
     import {mapActions, mapGetters} from 'vuex'
+    import VTooltip from 'v-tooltip'
+    Vue.use(VTooltip);
 
     export default {
+        data: () => {
+            return {
+                msg: 'Create a Card'
+            }
+        },
         computed: {
             ...mapGetters({
                 user: 'auth/user',
@@ -152,6 +160,98 @@
         }
         .team-photo {
             margin: 0 10px 0 -10px;
+        }
+    }
+    .tooltip {
+        display: block !important;
+        z-index: 10000;
+
+        .tooltip-inner {
+            background: black;
+            color: white;
+            border-radius: 16px;
+            padding: 5px 10px 4px;
+        }
+
+        .tooltip-arrow {
+            width: 0;
+            height: 0;
+            border-style: solid;
+            position: absolute;
+            margin: 5px;
+            border-color: black;
+        }
+
+        &[x-placement^="top"] {
+            margin-bottom: 5px;
+
+            .tooltip-arrow {
+                border-width: 5px 5px 0 5px;
+                border-left-color: transparent !important;
+                border-right-color: transparent !important;
+                border-bottom-color: transparent !important;
+                bottom: -5px;
+                left: calc(50% - 5px);
+                margin-top: 0;
+                margin-bottom: 0;
+            }
+        }
+
+        &[x-placement^="bottom"] {
+            margin-top: 5px;
+
+            .tooltip-arrow {
+                border-width: 0 5px 5px 5px;
+                border-left-color: transparent !important;
+                border-right-color: transparent !important;
+                border-top-color: transparent !important;
+                top: -5px;
+                left: calc(50% - 5px);
+                margin-top: 0;
+                margin-bottom: 0;
+            }
+        }
+
+        &[x-placement^="right"] {
+            margin-left: 5px;
+
+            .tooltip-arrow {
+                border-width: 5px 5px 5px 0;
+                border-left-color: transparent !important;
+                border-top-color: transparent !important;
+                border-bottom-color: transparent !important;
+                left: -5px;
+                top: calc(50% - 5px);
+                margin-left: 0;
+                margin-right: 0;
+            }
+        }
+
+        &[x-placement^="left"] {
+            margin-right: 5px;
+
+            .tooltip-arrow {
+                border-width: 5px 0 5px 5px;
+                border-top-color: transparent !important;
+                border-right-color: transparent !important;
+                border-bottom-color: transparent !important;
+                right: -5px;
+                top: calc(50% - 5px);
+                margin-left: 0;
+                margin-right: 0;
+            }
+        }
+
+        &[aria-hidden='true'] {
+            visibility: hidden;
+            opacity: 0;
+            transition: opacity .15s, visibility .15s;
+        }
+
+        &[aria-hidden='false'] {
+            visibility: visible;
+            opacity: 1;
+            transition: opacity .15s;
         }
     }
 </style>
