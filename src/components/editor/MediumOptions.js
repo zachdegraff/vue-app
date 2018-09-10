7256514b1e2a7776a81b1bf72f0dac09a8944cba
ahmetdoner7 <donerahmet093@gmail.com>
2018-09-10 13:04:17 +0400

    sep 10

--- src/components/editor/MediumOptions.js
+++ src/components/editor/MediumOptions.js
@@ -7,10 +7,10 @@ const MediumOptions = {
     },
     paste: {
         forcePlainText: false,
-        cleanPastedHTML: false,
+        cleanPastedHTML: true,
         cleanReplacements: [],
         cleanAttrs: ['class', 'style', 'dir'],
-        cleanTags: ['meta', 'table', 'tbody', 'thead', 'tr', 'td', 'th'],
+        cleanTags: ['meta', 'table', 'tbody', 'thead', 'tr', 'td', 'th', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
         unwrapTags: []
     },
     placeholder: false,
