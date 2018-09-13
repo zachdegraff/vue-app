70f6d7912311b227aa62588fbb94b90205d52ceb
ahmetdoner7 <donerahmet093@gmail.com>
2018-09-13 18:39:49 +0400

    not_underlined

--- src/pages/team/ManageTeamsPage.vue
+++ src/pages/team/ManageTeamsPage.vue
@@ -1,7 +1,12 @@
 <template>
     <q-page>
+        <div class="q-headline col-md-8 margin-bottom-20">
+
+        </div>
         <div class="row gutter-md q-pa-md">
+
             <div class="col-xs-4">
+                <router-link v-bind:to="'/'" tag='a' class='primary not_underlined'>← Return to Team Feed</router-link>
                 <teams-navigation />
             </div>
             <div class="col-xs-8">
@@ -20,3 +25,8 @@
         },
     }
 </script>
+<style>
+    .not_underlined{
+        text-decoration: none;
+    }
+</style>
